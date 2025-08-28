27ca4cbd91c41ac0e9285f94d71b1791a86f408b
Tanuja15-tech <bishttanuja15@gmail.com>
2025-08-28 10:24:14 +0530

    successfully enforced shadcn

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+ import { Button } from "@/components/ui/button";
 
 
 export default function Home() {
   return (
-  <h1 className="text-red-600"> hello world!</h1>
+  <Button>Hello World</Button>
   );
 }
